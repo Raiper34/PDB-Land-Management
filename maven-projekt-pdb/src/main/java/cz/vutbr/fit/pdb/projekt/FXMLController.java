f725f44fbc64b4a2b2b0b77845789d0f51fa0fbf
Jan Herec <xherec00@vutbr.cz>
2016-10-14 01:54:56 +0200

    Vytvořena základní struktura projektu, který je pose postaven na mavenu a platformě JAVAFX FXML. Kostra obsahuje také ukázku práce v prostředí JAVAFX FXML a ukázku vykreslení objektu z prostorové databáze na canvas. Jedná se o základní nástřel, pro představu.

---
+++ maven-projekt-pdb/src/main/java/cz/vutbr/fit/pdb/projekt/FXMLController.java
@@ -0,0 +1,86 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cz.vutbr.fit.pdb.projekt;
+
+import java.net.URL;
+import java.sql.SQLException;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.Initializable;
+
+import javafx.fxml.FXML;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.Label;
+import javafx.scene.paint.Color;
+import oracle.spatial.geometry.JGeometry;
+
+/**
+ * FXML Controller class
+ *
+ * @author jan
+ */
+public class FXMLController implements Initializable {  
+    
+    @FXML
+    private CheckBox checkBox;
+    
+    @FXML
+    private Label label;
+    
+    @FXML
+    private Canvas canvas;
+    
+    private GraphicsContext gc;
+    
+    private int clickedCount = 2;
+
+    @FXML
+    void checked(ActionEvent event) throws SQLException {
+        DatabaseTest databaseTest = new DatabaseTest();
+        JGeometry geom = databaseTest.databaseOperation();
+        
+        if (clickedCount % 2 != 0) {
+            gc.clearRect(0, 0, canvas.getWidth(), canvas.getHeight());
+            label.setText("Klikni, pro vykresleni obdelniku z databaze!");
+        }
+        else {
+            if (geom.isRectangle()) {
+                double x1 = geom.getOrdinatesArray()[0];
+                double y1 = geom.getOrdinatesArray()[1];
+
+                double x2 = geom.getOrdinatesArray()[2];
+                double y2 = geom.getOrdinatesArray()[3];
+
+
+                gc.setFill(Color.AQUA);
+                gc.setStroke(Color.BLACK);
+                gc.setLineWidth(10);
+
+                //gc.fill();
+                gc.strokeRect(x1, y2, x2 - x1, y2 - y1);
+                gc.fillRect(x1, y2, x2 - x1, y2 - y1);
+            }
+
+            label.setText("Obdelnik z databaze vykreslen!");
+        }
+        clickedCount++;
+    }
+    
+    /**
+     * Initializes the controller class.
+     */
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        gc = canvas.getGraphicsContext2D();
+    }  
+    
+    
+}
+
+
---
+++ maven-projekt-pdb/src/main/java/cz/vutbr/fit/pdb/projekt/NewFXMain.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cz.vutbr.fit.pdb.projekt;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+
+/**
+ *
+ * @author jan
+ */
+public class NewFXMain extends Application {
+    
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/FXML.fxml"));
+        
+        Scene scene = new Scene(root);
+        
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
