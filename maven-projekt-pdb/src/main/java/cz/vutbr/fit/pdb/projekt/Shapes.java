be24b0d369ac4163ef20e290f1b71177738ed56b
Marek <marek.marusic@gmail.com>
2016-10-28 17:18:45 +0200

    Created empty classes

---
+++ maven-projekt-pdb/src/main/java/cz/vutbr/fit/pdb/projekt/Shapes.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cz.vutbr.fit.pdb.projekt;
+
+/**
+ *
+ * @author archie
+ */
+public class Shapes {
+    //private List<ImprovedCircle> circles;
+    //private List<ImprovedPath> paths;
+    //private List<ImprovedPolygon> polygons;
+    public Shapes() {
+        //this.circles = 
+        //this.paths =
+        //this.polygons =
+    }
+}
---
+++ maven-projekt-pdb/src/main/java/cz/vutbr/fit/pdb/projekt/spatialEntity.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cz.vutbr.fit.pdb.projekt;
+
+import oracle.spatial.geometry.JGeometry;
+import java.util.Date;
+/**
+ *
+ * @author archie
+ */
+public class spatialEntity {
+    private int id;
+    private String name;
+    private String description; 
+    private String type; //Mozeme tu zadat akeho typu je to napr 1 = Pozemok, 2 = Dom, 3 = Strom atd...
+    private JGeometry geometry;
+    private Date valid_from;
+    private Date valid_to;
+    
+    public Shapes toShapes(){
+        Shapes shapes = new Shapes();
+        return shapes;
+    }
+    
+}
